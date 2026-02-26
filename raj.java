0b9463091a5add97278cc200a8912bece75de865
RajeshwariPrabhakar <vvce23ise0237@vvce.ac.in>
2026-02-26 12:47:29 +0530

    class to check even or odd numbers

--- raj.java
+++ raj.java
@@ -1,1 +1,11 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 14;
 
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
